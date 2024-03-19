6e96ed958c47e0ce447cbbf93e977b3e13daf974
EventManager <abhinavkl1412@gmail.com>
2024-03-19 22:18:21 +0530

    added a test method in Animal Abstract class

--- src/main/java/org/example/designpatterns/factory/animal/Animal.java
+++ src/main/java/org/example/designpatterns/factory/animal/Animal.java
@@ -2,4 +2,9 @@ package org.example.designpatterns.factory.animal;
 
 public abstract class Animal {
     public abstract void eat();
+
+    public void test()
+    {
+        System.out.print("test");
+    }
 }
